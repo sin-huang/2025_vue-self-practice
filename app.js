924aa8b97dd20446138e12c7b0ecad96fa1a53cf
melody <melody880306@gmail.com>
2025-04-28 22:53:53 +0800

    feature: automatically card layout

--- app.js
+++ app.js
@@ -53,61 +53,71 @@ Vue.createApp({
             bathhub: true,
             breakfast: true,
           },
-        },{
-            chinName: "豪華四人房",
-            engName: "Luxary Four Room",
-            price: 8000,
-            available: 1,
-            cover: null,
-            discount: 0.75,
-            equipment: {
-              wifi: false,
-              bathhub: true,
-              breakfast: true,
-            },
-        },{
-            chinName: "總統套房",
-            engName: "President Suite",
-            price: 10000,
-            available: 1,
-            cover: null,
-            discount: 0.95,
-            equipment: {
-              wifi: true,
-              bathhub: true,
-              breakfast: true,
-            }
-        },{
-            chinName: "商務單人房",
-            engName: "Business One Room",
-            price: 1500,
-            available: 10,
-            cover: null,
-            discount: 0.75,
-            equipment: {
-              wifi: false,
-              bathhub: true,
-              breakfast: true,
-            }
-        },{
-            chinName: "典雅景觀房",
-            engName: "Elegant Landscpae Room",
-            price: 6500,
-            available: 3,
-            cover: null,
-            discount: 0.7,
-            equipment: {
-              wifi: false,
-              bathhub: true,
-              breakfast: true,
-            }
-        }
+        },
+        {
+          chinName: "豪華四人房",
+          engName: "Luxary Four Room",
+          price: 8000,
+          available: 1,
+          cover: null,
+          discount: 0.75,
+          equipment: {
+            wifi: false,
+            bathhub: true,
+            breakfast: true,
+          },
+        },
+        {
+          chinName: "總統套房",
+          engName: "President Suite",
+          price: 10000,
+          available: 1,
+          cover: null,
+          discount: 0.95,
+          equipment: {
+            wifi: true,
+            bathhub: true,
+            breakfast: true,
+          },
+        },
+        {
+          chinName: "商務單人房",
+          engName: "Business One Room",
+          price: 1500,
+          available: 10,
+          cover: null,
+          discount: 0.75,
+          equipment: {
+            wifi: false,
+            bathhub: true,
+            breakfast: true,
+          },
+        },
+        {
+          chinName: "典雅景觀房",
+          engName: "Elegant Landscpae Room",
+          price: 6500,
+          available: 3,
+          cover: null,
+          discount: 0.7,
+          equipment: {
+            wifi: false,
+            bathhub: true,
+            breakfast: true,
+          },
+        },
       ],
     };
   },
   computed: {
-    finalPrice() {
-      return this.rooms[0].price * this.rooms[0].discount;
+    chunkedRooms() {
+      const chunkSize = 2;
+      // 因為slice是淺拷貝 不會影響原陣列 所以初始化一個新陣列準備裝切割後的結果
+      const chunks = [];
+      for (let i = 0; i < this.rooms.length; i += chunkSize) {
+        chunks.push(this.rooms.slice(i, i + chunkSize));
+      }
+      return chunks;
     },
   },
 }).mount("#app");
